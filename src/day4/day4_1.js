e3d89287bd862d8d832f353a9baf39d6c6d5b872
unknown <dgerrells@hotmail.com>
2020-05-09 20:22:32 -0700

    add parsing of file with regex to array of data

--- src/day4/day4_1.js
+++ src/day4/day4_1.js
@@ -1,3 +1,10 @@
-export const getSectorNumber = (stringCode) => {
-  throw 'not done yet';
+const PARSE_DATA = /(?<data>[a-zA-Z0-9-]+)(?<sector>(?<=-)[0-9]+)\[(?<checksum>(?<=\[)[a-zA-Z]{5}(?=\]))/g;
+const INVALID_NAME_CHARS = /[^a-zA-Z]*/g;
+
+export const getSectorData = (inputStr) => {
+  const dataArray = inputStr.matchAll(PARSE_DATA);
+  return [...dataArray].map((match) => ({
+    ...match.groups,
+    data: match.groups.data.replace(INVALID_NAME_CHARS, ''),
+  }));
 };
--- src/day4/day4_1.test.js
+++ src/day4/day4_1.test.js
@@ -1,8 +1,53 @@
 import test from 'ava';
-import { getSectorNumber } from './day4_1.js';
+import { getSectorData } from './day4_1.js';
 
-test('traverse should be pure', (t) => {
-  const expectSecNum = 123;
+test('should parse sector data from string', (t) => {
+  const expectedData = {
+    checksum: 'abxyz',
+    data: 'aaaaabbbzyx',
+    sector: '123',
+  };
+
   const sectorData = 'aaaaa-bbb-z-y-x-123[abxyz]';
-  t.deepEqual(getSectorNumber(sectorData), expectSecNum);
+  const dataArray = getSectorData(sectorData);
+  t.true(dataArray.length === 1, 'should find 1 match');
+  t.deepEqual(dataArray[0], expectedData);
+});
+
+test('should parse multiple sector data from input string', (t) => {
+  const arraySize = 100;
+  const sectorDataObj = {
+    checksum: 'qrazx',
+    data: 'wifilzofwbiwifunyyhachyylcha',
+    sector: '526',
+  };
+  const expectData = Array.from(new Array(arraySize).fill(0), () => ({
+    ...sectorDataObj,
+  }));
+  const sectorData = 'wifilzof-wbiwifuny-yhachyylcha-526[qrazx]';
+  const inputString = new Array(arraySize).fill(sectorData).join('\n');
+
+  const dataArray = getSectorData(inputString);
+  t.true(dataArray.length === arraySize, 'should find all matches');
+  t.deepEqual(dataArray, expectData);
+});
+
+test('should return empty with unmatched input sector data', (t) => {
+  const sectorData = [
+    'I am the bad string :( no sector gere',
+    'i-am-still-bad[cause]',
+    '3213-123-3-23[12345]',
+  ];
+
+  const dataArray = getSectorData(sectorData.join('\n'));
+  t.true(dataArray.length === 0, 'should have no matches');
+});
+
+test('should remove any numbers in encrypted name', (t) => {
+  // there is the case where numbers or characters are added to
+  const sectorData = ['321-df-34-1234[rtete]'];
+
+  const dataArray = getSectorData(sectorData.join('\n'));
+  t.true(dataArray.length === 1, 'should have one match');
+  t.deepEqual(dataArray[0].data, 'df');
 });
